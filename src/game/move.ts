38d85746f02e30bcadaff40f9ea0d370bd5e261e
Mikhail Bondarev <mikhail.bondarev@onetwotrip.com>
2023-04-05 17:26:55 +0200

    init

---
+++ src/game/move.ts
@@ -0,0 +1,53 @@
+export class Vector {
+  public x: number;
+  public y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  public next(velocity: Vector): Vector {
+    return new Vector(this.x + velocity.x, this.y + velocity.y);
+  }
+}
+
+export interface Movable {
+  getPosition(): Vector;
+  getVelosity(): Vector;
+  setPosition(newV: Vector): void;
+}
+
+export class Move {
+  public m: Movable;
+  constructor(m: Movable) {
+    this.m = m;
+  }
+  public execute(): void {
+    try {
+      this.m.setPosition(this.m.getPosition().next(this.m.getVelosity()));
+    } catch (error) {
+      throw new Error('can not move');
+    }
+  }
+}
+
+export interface IUObject {
+  getProperty(key: string): Vector | number;
+  setProperty(key: string, newValue: object): void;
+}
+
+export class MovableAdapter implements Movable {
+  public o: IUObject;
+  constructor(o: IUObject) {
+    this.o = o;
+  }
+
+  getPosition(): Vector {
+    return this.o.getProperty('position') as Vector;
+  }
+  getVelosity(): Vector {
+    return this.o.getProperty('velocity') as Vector;
+  }
+  setPosition(newV: Vector): void {
+    this.o.setProperty('position', newV);
+  }
+}
---
+++ src/game/rotate.test.ts
@@ -0,0 +1,39 @@
+import { IUObject, Rotate, RotateAdapter } from './rotate';
+
+let ship: IUObject;
+
+describe('rotatable', () => {
+  const getDirectionMock = jest
+    .spyOn(RotateAdapter.prototype, 'getDirection')
+    .mockImplementationOnce(() => 3);
+
+  const getVelocityMock = jest
+    .spyOn(RotateAdapter.prototype, 'getAngularVelosity')
+    .mockImplementationOnce(() => 2);
+
+  const setPositionMock = jest
+    .spyOn(RotateAdapter.prototype, 'setDirection')
+    .mockImplementation(() => undefined);
+
+  it('3 + 2 = 5', () => {
+    const m = new Rotate(new RotateAdapter(ship));
+    m.execute();
+    expect(getDirectionMock).toHaveBeenCalled();
+    expect(getVelocityMock).toHaveBeenCalled();
+    expect(setPositionMock).toHaveBeenCalledWith(5);
+  });
+
+  it('not velocity', () => {
+    const m = new Rotate(new RotateAdapter(ship));
+    expect(getDirectionMock).toHaveBeenCalled();
+    expect(getVelocityMock).toHaveBeenCalled();
+    expect(() => m.execute()).toThrow(new Error('can not rotate'));
+  });
+
+  it('not position', () => {
+    const m = new Rotate(new RotateAdapter(ship));
+    expect(getDirectionMock).toHaveBeenCalled();
+    expect(getVelocityMock).toHaveBeenCalled();
+    expect(() => m.execute()).toThrow(new Error('can not rotate'));
+  });
+});
